cee76b70c8d7c5e2708359ce53ae9656a0c3937d
Fathur Rahman <fathur.rahman@udana.id>
2022-04-25 18:48:56 +0700

    Admin Foto Produk

--- app/Models/ProductGallery.php
+++ app/Models/ProductGallery.php
@@ -7,7 +7,6 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ProductGallery extends Model
 {
-    //
     use SoftDeletes;
 
     protected $fillable = [
--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,7 @@ Route::get('/', 'DashboardController@index')->name('dashboard');
 Auth::routes(['register' => false]);
 
 // Route::get('/home', 'HomeController@index')->name('home');
+Route::get('products/{id}/gallery', 'ProductController@gallery')->name('products.gallery');
 Route::resource('products', 'ProductController');
+
+Route::resource('product-galleries', 'ProductGalleryController');
